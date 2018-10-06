396fc9ef7818f18d81ff0c215cd76c4e7523ccf0
SimBed <dansimbed@gmail.com>
2018-10-06 14:04:36 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,8 @@ class SessionsController < ApplicationController
       log_in user
       #compact code using ternary operator equivalent to if......else
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      #allows for friendly forwarding...previously redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "SimBed",
+             email: "dansimbed@gmail.com",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true)
+             
+User.create!(name:  "Fluke",
+             email: "example-4@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: false)
+
+0.upto(2) do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
+
+4.upto(98) do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
